b7acd3424248abbc3786ff45484a5b5638bb587a
System Administrator <root@admindeMacBook-Pro.local>
2016-03-03 17:03:21 +0800

    add zabbix

--- log-analyse.py
+++ log-analyse.py
@@ -8,23 +8,19 @@ asp = {}
 remote_addr =[]
 coohua_id = []
 
-def func(args,dire,fis):	#回调函数的定义
+def func(args,dire,fis):	#回调函数的定义walk(root,callable,args)方法有三个参数：要遍历的目录，回调函数，回调函数的参数（元组形式
     for f in fis:
         dir = dire + s + f
         print dir
         #t = os.popen("zcat %s|grep 'asp.do' " % dir)
-        t = os.popen("zcat %s|grep 'getAuthCode.do' " % dir)
-        for status in  t.readlines():
-            t = json.loads(status)
-            #remote_addr.append(t["remote_addr"])
-            #coohua_id.append(t["coohua_id"])
-            #asp[t["coohua_id"]] = t["remote_addr"]
-           # file = open('log-asp1.txt', 'a')
-            file = open('getAuthCode.txt', 'a')
+        t = os.popen("zcat %s|grep 'getAuthCode.do' " % dir) #os.popen和os.system()一样执行命令的
+        for status in  t.readlines(): #按行读
+            t = json.loads(status) #解析json
+            file = open('getAuthCode.txt', 'a') #追加到打开文件
             file.write("%s:%s:%s \n" %(t["request_time"],t["coohua_id"],t["remote_addr"]))
             file.close( )
 
-os.path.walk(root,func,())
+os.path.walk(root,func,())#调用
 
 #for k,v in asp.items():
 #    print k,v
